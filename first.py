593eb21eba155995c9050c7436f06f038c0d85f1
shivanikhalote <shivanikhalote1999@gmail.com>
2023-08-22 14:55:06 +0530

    Variabls

--- first.py
+++ first.py
@@ -1,6 +1,3 @@
-a = 256
-if a%16==0:
-    print("divisible of 16")
-else:
-    print("NOT divisible of 16")
-print("OUT OF LOOP")
+name ="shivani"
+surname = "khalote"
+print("my name is {} {}".format(name,surname))
