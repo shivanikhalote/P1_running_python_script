14540f7457e3be5e7b16ab4dfc8e0ecf1ab584d9
shivanikhalote <shivanikhalote1999@gmail.com>
2023-08-29 14:02:51 +0530

    Performing Arithemetic Operation

---
+++ ari_op.py
@@ -0,0 +1,14 @@
+# arithemetic operation
+a = 12
+b = 120
+add = a + b
+sub = a - b
+mul = a * b
+mod = b % a
+
+print('Arthematic operation of number 12 and 120')
+print('addition = {}'.format(add))
+print('subtraction = {}'.format(sub))
+print('multiplication = {}'.format(mul))
+print('reminder = {}'.format(mod))
+
