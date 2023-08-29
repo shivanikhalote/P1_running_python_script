c00c55d64aca6da44edfd5d0fa31627bbe46879d
shivanikhalote <shivanikhalote1999@gmail.com>
2023-08-29 14:28:20 +0530

    append and insertion operation in list

--- list.py
+++ list.py
@@ -4,3 +4,12 @@ cities =["Nagpur","pune","Indore","Bhopal"]
 print('third position in list = {}'.format(cities[2]))
 
 print(cities)
+
+# Append in a list
+cities.append("chhindwara")
+print('appended list = {}'.format(cities))
+
+# inserting at choosen position
+cities.insert(3,"Shrinagar")
+print('inserted value in a list  = {}'.format(cities))
+
